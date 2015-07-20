1734e3cf381c11c9a2a34aba5a6eb60eeab76ea4
josephferrairo <josephferrairo@gmail.com>
2015-07-20 00:14:16 +0000

    added input fuction

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,6 @@
+
+
+
 class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
@@ -6,4 +9,16 @@ class QuotesController < ApplicationController
 	def new
 		@quote = Quote.new
 	end
+
+	def create
+		Quote.create(quote_params)
+		redirect_to root_path
+	end
+
+private 
+
+def quote_params 
+	params.require(:quote).permit(:saying, :author)
 end
+end
+
