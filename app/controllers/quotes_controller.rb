ec52542cd7afa77ef097f5f3b6541d55640c902d
josephferrairo <josephferrairo@gmail.com>
2015-08-10 01:53:11 +0000

    added controller test

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -14,14 +14,20 @@ class QuotesController < ApplicationController
 		end
 		redirect_to root_path
 	end
-	
+
 	def about
 	end
 
-private 
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "Not Found", :status => :not_found
+		end
+	end
 
-def quote_params 
+private
+
+def quote_params
 	params.require(:quote).permit(:saying, :author)
 end
 end
-
